803e570d378c4b52183e51f0c1fd8e5208a465f4
Mahsa M <mm3424@rit.edu>
2023-11-29 00:42:39 -0500

    codes added

--- modules/losses.py
+++ modules/losses.py
@@ -76,3 +76,104 @@ class CorrectnessPredictionLoss(nn.Module):
             return self.loss_func(confidences, correctness) * correctness.size(1)
         else:
             return self.loss_func(confidences, correctness)
+        
+# Added: GFL loss
+@registry.register_loss("gfl_logit_bce")
+class GFLLogitBinaryCrossEntropy(nn.Module):
+    """Returns Binary Cross Entropy for logits.
+
+    Attention:
+        `Key`: logit_bce
+    """
+    
+    def __init__(self,**params):
+        super().__init__()
+        self.gfl_lambda = params.get("lambda", 1)
+
+    def forward(self, sample_list, model_output):
+        """Calculates and returns the re-weighted binary cross entropy for logits
+
+        Args:
+            sample_list (SampleList): SampleList containing `targets` attribute.
+            model_output (Dict): Model output containing `scores` attribute.
+
+        Returns:
+            torch.FloatTensor: Float value for loss.
+
+        """
+        scores = model_output["scores"]
+        targets = sample_list["targets"]
+        
+        loss = F.binary_cross_entropy_with_logits(scores, targets, reduction="none")
+        loss = torch.sum(loss,dim=1)
+        mx = torch.max(loss)
+        
+        # re-weighting for GFL
+        L = (loss - mx)/self.gfl_lambda
+        Z = torch.exp(L)/torch.sum(torch.exp(L)) #nom/denom
+        
+        return torch.sum(Z*loss)
+    
+
+@registry.register_loss("gfl_triple_logit_bce")
+class GFLTripleLogitBinaryCrossEntropy(nn.Module):
+    """
+    This is used for Three-branch fusion only. We predict scores and compute
+    cross entropy loss for each of branches.
+    """
+
+    def __init__(self,**params):
+        super().__init__()
+        self.dro_lambda = params.get("lambda", 1)
+
+    def forward(self, sample_list, model_output):
+        """Calculates and returns the re-weighted binary cross entropy for logits
+        Args:
+            sample_list (SampleList): SampleList containing `targets` attribute.
+            model_output (Dict): Model output containing `scores` attribute.
+        Returns:
+            torch.FloatTensor: Float value for loss.
+        """
+        scores = model_output["scores"]
+        targets = sample_list["targets"]
+
+        if scores.dim() == 3:
+            loss1 = F.binary_cross_entropy_with_logits(
+                    scores[:, 0], targets, reduction="none"
+                )
+            loss1 = torch.sum(loss1,dim=1)
+            mx = torch.max(loss1)
+            L = (loss1 - mx)/self.dro_lambda
+            Z = torch.exp(L)/torch.sum(torch.exp(L))
+            loss1 = torch.sum(Z*loss1)
+
+            loss2 = F.binary_cross_entropy_with_logits(
+                    scores[:, 1], targets, reduction="none"
+                )
+            loss2 = torch.sum(loss2,dim=1)
+            mx = torch.max(loss2)
+            L = (loss2 - mx)/self.dro_lambda
+            Z = torch.exp(L)/torch.sum(torch.exp(L))
+            loss2 = torch.sum(Z*loss2)
+
+            loss3 = F.binary_cross_entropy_with_logits(
+                    scores[:, 2], targets, reduction="none"
+                )
+            loss3 = torch.sum(loss3,dim=1)
+            mx = torch.max(loss3)
+            L = (loss3 - mx)/self.dro_lambda
+            Z = torch.exp(L)/torch.sum(torch.exp(L))
+            loss3 = torch.sum(Z*loss3)
+            
+            loss = loss1 + loss2 + loss3
+        else:
+            loss = F.binary_cross_entropy_with_logits(scores, targets, reduction="none")
+            loss = torch.sum(loss,dim=1)
+            mx = torch.max(loss)
+            L = (loss - mx)/self.dro_lambda
+            Z = torch.exp(L)/torch.sum(torch.exp(L))
+            loss = torch.sum(Z*loss)
+
+        return loss
+
+# End of GFL loss
